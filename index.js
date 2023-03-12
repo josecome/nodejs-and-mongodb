2008565207930064e27ddaa0a80d3ae8638e7fdd
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-12 17:49:24 +0200

    Code to connect and get data from mongodb added

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var http = require('http');
 var Mongo = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
 var rst = "";
+const url= 'mongodb://localhost:27017';
+const dtb='Test'
+const client= new Mongo(url);
 
-Mongo.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("Test");
-  var query = { country: "England" };
-  dbo.collection("players").find(query).toArray(function(err, result) {
-    if (err) throw err;
-    rst = result;
-    console.log(result);
-    db.close();
-  });
-});
+async function process()
+{
+  let r = await client.connect();
+  db = r.db(dtb);
+  c = db.collection('players');
+  f = {country: "England"}
+  let d = await c.find(f).toArray();
+  rst = JSON.stringify(d);
+  console.log(d)
+}
+process();
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  console.log("rst: " + rst);
   res.end(rst);
 }).listen(8080);
